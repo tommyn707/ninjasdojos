c326c4acde75a5cea36390206f039a28f7fcf1d1
tommyn707 <tommy.n707@gmail.com>
2020-02-14 11:43:14 -0800

    first commit

---
+++ dojo_ninjas_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ninjas(models.Model):
+    dojo_id = models.ForeignKey(dojos, related_name = 'enrolled', on_delete = models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
